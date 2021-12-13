e6a09f6a5aa596a9f16a6c1128c9cba5fa3de02a
tsoyroman <tsoyroman5@gmail.com>
2021-12-13 22:28:25 +0900

    Commit

--- src/main/java/jm/task/core/jdbc/util/HibernateSessionFactoryUtil.java
+++ src/main/java/jm/task/core/jdbc/util/HibernateSessionFactoryUtil.java
@@ -1,6 +1,5 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.cj.Session;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
